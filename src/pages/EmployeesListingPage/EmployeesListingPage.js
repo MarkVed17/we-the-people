2b622100bd90ab25d9862a67bdf8bc8c12b94f85
MarkVed17 <vedantlahane17@gmail.com>
2022-09-19 02:31:41 +0530

    added a link to `Employee Hierarchy Page`

--- src/pages/EmployeesListingPage/EmployeesListingPage.js
+++ src/pages/EmployeesListingPage/EmployeesListingPage.js
@@ -51,6 +51,10 @@ const EmployeesListingPage = () => {
 
   return (
     <div className="employees-table">
+      <button onClick={() => navigate("/overview")} className="link">
+        <span class="material-symbols-outlined">visibility</span>
+        <span>View Employee Hierarchy</span>
+      </button>
       <DataGrid
         rows={rows}
         columns={columns}
