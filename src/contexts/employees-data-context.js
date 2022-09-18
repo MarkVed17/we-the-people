9c91bd02ebbd27a914a5dcc50bf5fa10fa11ebcd
MarkVed17 <vedantlahane17@gmail.com>
2022-09-18 21:53:46 +0530

    removed log statements

--- src/contexts/employees-data-context.js
+++ src/contexts/employees-data-context.js
@@ -14,7 +14,6 @@ const EmpDataProvider = ({ children }) => {
           "https://opensheet.elk.sh/1gH5Kle-styszcHF2G0H8l1w1nDt1RhO9NHNCpHhKK0M/employees"
         );
         const data = await response.json();
-        console.log(data);
         setEmpData(data);
         setLoading(false);
       } catch (err) {
