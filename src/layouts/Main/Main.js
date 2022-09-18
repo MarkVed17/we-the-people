02579f7c35e04020fd613d072e939453601fb9ea
MarkVed17 <vedantlahane17@gmail.com>
2022-09-19 02:30:46 +0530

    created a route for `Employee Hierarchy Page`

--- src/layouts/Main/Main.js
+++ src/layouts/Main/Main.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import { EmployeeDetailsPage, EmployeesListingPage } from "../../pages";
+import { EmployeeDetailsPage, EmployeeHierarchyPage, EmployeesListingPage } from "../../pages";
 
 const Main = () => {
   return (
     <Routes>
       <Route path="/" element={<EmployeesListingPage />} />
       <Route path="/employee/:name" element={<EmployeeDetailsPage />} />
+      <Route path="/overview" element={<EmployeeHierarchyPage />} />
     </Routes>
   );
 };
