2bee1d0b8f0508e3208d67fac566e758c2e705f1
MarkVed17 <vedantlahane17@gmail.com>
2022-09-18 13:30:34 +0530

    provided `employees data` context to the App

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { EmpDataProvider } from "./contexts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <EmpDataProvider>
+        <App />
+      </EmpDataProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
