ceb9d02f4b3d8a22329184796aa978942772471c
MarkVed17 <vedantlahane17@gmail.com>
2022-09-19 02:31:15 +0530

    barrel exports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { EmployeesListingPage } from "./EmployeesListingPage/EmployeesListingPage";
 export { EmployeeDetailsPage } from "./EmployeeDetailsPage/EmployeeDetailsPage";
+export { EmployeeHierarchyPage } from "./EmployeeHierarchyPage/EmployeeHierarchyPage";
